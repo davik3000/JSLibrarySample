7cefc644c0f75579186814a9cf5a8a00add93d0b
davik3000 <davik3000@users.noreply.github.com>
2017-06-09 17:40:59 +0200

    Created a little experiment
    Added main.min.js, generated by Netbeans plugin "JS CSS Minify Compress"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+window.MyExperiment = {
+    name: "",
+    type: "",
+    getName: function () {
+        return name;
+    },
+    getType: function () {
+        return type;
+    }
+}
 
+var myExp = new window.MyExperiment();
+var name = myExp.getName();
+var type = myExp.getType();
